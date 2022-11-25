62cad8583fd8d9ebbf1ea84077ce34fd21d054e0
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-11-25 16:33:00 +0000

    studying union types

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,23 @@
-console.log("Osvaldo");
+//explicit types
+var nome;
+var age;
+var isActive;
+nome = "Osvaldo";
+age = 18;
+isActive = true;
+//
+//Union types
+var uid;
+uid = 123;
+uid = "123";
+var names = [];
+names.push("Osvaldo");
+names.push(10);
+names.push(true);
+var user;
+user = {
+    id: 1,
+    name: "Osvaldo"
+};
+var user2;
+user2 = { id: 1, name: "Jorge" };
--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,35 @@
-console.log("Osvaldo")
+
+//explicit types
+let nome: string;
+let age: number;
+let isActive: boolean;
+
+
+nome = "Osvaldo"
+age = 18;
+isActive = true;
+//
+
+//Union types
+let uid: string | number;
+uid = 123;
+uid = "123"
+
+let names: (number | string | boolean)[] = []
+names.push("Osvaldo")
+names.push(10)
+names.push(true)
+
+let user: object;
+
+user = {
+    id: 1,
+    name: "Osvaldo"
+}
+
+let user2: {
+    id: number,    
+    name: string,
+}
+
+user2 = {id:1, name:"Jorge"}
